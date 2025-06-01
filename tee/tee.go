b67c5583738e20ef2b784fc150e6ac2f093ba45f
Nikita-Valahanovich <nikita.valahanovich@gmail.com>
2025-06-01 14:12:30 +0300

    Added comments tee pattern

--- tee/tee.go
+++ tee/tee.go
@@ -1,3 +1,4 @@
+// Паттерн Tee
 package main
 
 import (
@@ -5,64 +6,65 @@ import (
 	"sync"
 )
 
+// tee - паттерн дублирования данных из входного канала в 2 других
 func tee(in <-chan int) (_, _ <-chan int) {
-	out1 := make(chan int)
-	out2 := make(chan int)
+	out1 := make(chan int) // создаем выходной канал №1
+	out2 := make(chan int) // создаем выходной канал №2
 
 	go func() {
-		defer close(out1)
-		defer close(out2)
+		defer close(out1) // закрываем канал после завершения работы функции
+		defer close(out2) // закрываем канал после завершения работы функции
 
-		for v := range in {
-			var out1, out2 = out1, out2
+		for v := range in { // ренжом получаем данные из входного канала
+			var out1, out2 = out1, out2 // делаем затенение
 			for i := 0; i < 2; i++ {
 				select {
-				case out1 <- v:
-					out1 = nil
-				case out2 <- v:
-					out2 = nil
+				case out1 <- v: // записываем данные в канал 1
+					out1 = nil // блокируем этот канал после записи
+				case out2 <- v: // записываем данные в канал 2
+					out2 = nil // блокируем этот канал после записи
 				}
 			}
 		}
 	}()
 
-	return out1, out2
+	return out1, out2 // возвращаем данные из каналов
 }
 
-// генератор
+// generate - паттерн генератора
 func generate() chan int {
-	ch := make(chan int)
+	ch := make(chan int) // создаем канал, в которой записываем данные
 
 	go func() {
-		defer close(ch)
+		defer close(ch) // закрываем канал, после завершения goroutine
 
 		for i := 1; i <= 5; i++ {
-			ch <- i
+			ch <- i // записываем значение в канал (от 1 до 5)
 		}
 	}()
 
-	return ch
+	return ch // возвращаем данные из канала
 }
 
 func main() {
 	ch1, ch2 := tee(generate())
-	wg := &sync.WaitGroup{}
+	wg := &sync.WaitGroup{} // примитив синхронизации
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
+	wg.Add(1)   // создаем счетчик
+	go func() { // создаем точку fork, запускаем goroutine
+		defer wg.Done() // снижаем счетчик на 1, после завершения работы функции
 		for v := range ch1 {
 			fmt.Println("channel 1 = ", v)
 		}
 	}()
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
+	wg.Add(1)   // создаем счетчик
+	go func() { // создаем точку fork, запускаем goroutine
+		defer wg.Done() // снижаем счетчик на 1, после завершения работы функции
 		for v := range ch2 {
 			fmt.Println("channel 2 = ", v)
 		}
 	}()
 
-	wg.Wait()
+	wg.Wait() // создаем точку Join, дожидаемся завершения работы горутин
 }
